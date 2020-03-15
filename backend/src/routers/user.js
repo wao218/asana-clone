c197e033c70fd6a55bad72293a9e42ae31ce7bc6
Wesley Osborne <wao218@lehigh.edu>
2020-03-14 21:14:22 -0400

    Added logoutAll accounts route

--- backend/src/routers/user.js
+++ backend/src/routers/user.js
@@ -45,4 +45,15 @@ router.post('/users/logout', auth, async (req, res) => {
   }
 });
 
+// Logout of all users accounts
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
 module.exports = router;
