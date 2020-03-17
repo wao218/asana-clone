79678f25bc05687ba64d8d27c7e57e282861a1d9
Wesley Osborne <wao218@lehigh.edu>
2020-03-16 22:50:03 -0400

    Added Create Workspace route and Read all Workspaces Route

--- backend/src/routers/workspace.js
+++ backend/src/routers/workspace.js
@@ -4,7 +4,7 @@ const auth = require('../middleware/auth');
 const router = new express.Router();
 
 // Create an Workspace
-router.post('/workspace', auth, async (req, res) => {
+router.post('/workspaces', auth, async (req, res) => {
   const workspace = new Workspace({
     ...req.body,
   });
@@ -16,7 +16,18 @@ router.post('/workspace', auth, async (req, res) => {
     res.status(400).send(e);
   }
 });
-// Read an Workspace
+// Read all Workspaces
+router.get('/workspaces', auth, async (req, res) => {
+  try {
+    const workspaces = await Workspace.find();
+
+    res.send(workspaces)
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+// Read an individual Workspace
 
 // Update an Workspace
 
