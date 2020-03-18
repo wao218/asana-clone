b8c7fb7608ea674d0bcc703b6ac17bdf2f07ba03
Wesley Osborne <wao218@lehigh.edu>
2020-03-17 20:56:47 -0400

    Added team member field to team model

--- backend/src/models/team.js
+++ backend/src/models/team.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const teamSchema = new mongoose.Schema({
   name: {
@@ -10,7 +11,25 @@ const teamSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     required: true,
     ref: 'Workspace'
-  }
+  },
+  team_members: [
+    {
+      email: {
+        type: String,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+          if (!validator.isEmail(value)) {
+            throw new Error('Email is invalid');
+        }
+      }
+    },
+    name: {
+      type: String,
+      trim: true
+    }
+  }]
 }, {
   timestamps: true
 });
---
+++ backend/src/routers/team.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const Team = require('../models/team');
+const auth = require('../middleware/auth');
+const router = new express.Router();
+
+// Creating a new team
+
+// Reading all teams
+
+// Reading a single team
+
+// Updating a team
+
+// Deleting a team
